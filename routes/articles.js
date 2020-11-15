1bfe37c8aeb89d281b5800ef545f1c847d62c288
prateek <princeshekhawat78@gmail.com>
2020-11-15 17:50:01 +0530

    correct delete op

--- routes/articles.js
+++ routes/articles.js
@@ -110,18 +110,19 @@ router.delete("/:id", (req, res) => {
 
   let query = { _id: req.params.id };
 
-  Article.findById(req.params.id),
+  Article.findById(req.params.id,
     (err, article) => {
       if (article.author != req.user._id) {
         res.status(500).send();
-      } 
+      } else {
         Article.remove(query, (err) => {
           if (err) {
             console.log(err);
           }
           res.send("Success");
         });
-    };
+      }
+    });
 });
 
 // Show Articles Route Brad Code
